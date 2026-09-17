dc1f73070e61b081511e7d0c7b6088acf4f9de2b
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-09-16 21:10:55 -0500

    style: work on style of Duolingo style button

--- Frontend/src/components/button/Button.tsx
+++ Frontend/src/components/button/Button.tsx
@@ -5,10 +5,9 @@ const Button = ({children, ...prop}: ButtonHTMLAttributes<HTMLButtonElement>) =>
     return (
         <button
             {...prop}
-            style={{position: "relative", cursor: "pointer"}}
-            className="group"
+            className="group relative cursor-pointer"
         >
-            <div className="bg-blue_400 py-[.2em] px-[2em] border-3 border-blue_200 relative z-10 -translate-y-[.6rem] group-active:translate-y-0 rounded-[.6em]">
+            <div className="bg-blue_400 py-[.2em] px-[2em] border-3 border-blue_200 relative z-10 -translate-y-[.6rem] group-active:translate-y-0 rounded-[.6em] w-full">
                 {children}
             </div>
             <div className="w-full h-full absolute bg-blue_200 top-0 left-0 rounded-[.6em]"></div>
